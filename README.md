912fa1118cb3c2619286b63163b804540d8eb379
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-28 15:43:36 +0100

    ai-connector-alexa

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Dependency Status](https://david-dm.org/dragonprojects/ai-connector-alexa.svg)](https://david-dm.org/dragonprojects/ai-connector-alexa)
+[![devDependency Status](https://david-dm.org/dragonprojects/ai-connector-alexa/dev-status.svg)](https://david-dm.org/dragonprojects/ai-connector-alexa?type=dev)
---
+++ src/web.js
@@ -0,0 +1,32 @@
+require('dotenv').config({ silent: true });
+
+const app = require('express')();
+app.listen(process.env.PORT);
+
+const rp = require('request-promise').defaults({
+  json: true,
+  url: process.env.AI_WEBHOOK_URL,
+});
+
+app.post('/', require('body-parser').json(), async (req, res) => {
+  const alexaRequest = req.body;
+  const aiRequest = {
+    intent: alexaRequest.request.intent.name,
+    params: (() => {
+      const params = {};
+      const slots = alexaRequest.request.intent.slots;
+      if (slots) {
+        for (const name of Object.keys(slots)) {
+          params[name] = slots[name].value;
+        }
+      }
+      return params;
+    })(),
+  };
+  const aiResponse = await rp.post({ body: aiRequest });
+  const alexaResponse = {
+    outputSpeech: { type: 'PlainText', text: aiResponse.say },
+    card: { type: 'Standard', title: aiResponse.display.title, text: aiResponse.display.text },
+  };
+  res.send(alexaResponse);
+});
