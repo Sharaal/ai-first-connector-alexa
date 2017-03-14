87d94a4ec278ef0f7acd299710a17fcdd12e75e7
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-14 13:04:31 +0100

    Use "dcontrollers" and "dexpress"

--- src/web.js
+++ src/web.js
@@ -1,58 +1,7 @@
 require('dotenv').config({ silent: true });
-const _ = require('lodash');
 
-const app = require('express')();
-app.listen(process.env.PORT);
-
-const rp = require('request-promise').defaults({
-  json: true,
-  url: process.env.AI_WEBHOOK_URL,
-});
-
-app.post('/', require('body-parser').json(), async (req, res) => {
-  const alexaRequest = req.body;
-  const id = _.get(alexaRequest, 'request.requestId');
-  console.log(`id: ${id}`);
-  console.log(`(${id}) alexaRequest: ${JSON.stringify(alexaRequest)}`);
-  const type = _.get(alexaRequest, 'request.type');
-  if (type !== 'IntentRequest') {
-    res.send();
-    return;
-  }
-  const aiRequest = {
-    id,
-    locale: _.get(alexaRequest, 'request.locale'),
-    name: _.get(alexaRequest, 'request.intent.name'),
-    params: (() => {
-      const params = {};
-      const slots = _.get(alexaRequest, 'request.intent.slots', {});
-      for (const name of Object.keys(slots)) {
-        params[name] = slots[name].value;
-      }
-      return params;
-    })(),
-    session: _.get(alexaRequest, 'session.attributes', {}),
-  };
-  console.log(`(${id}) aiRequest: ${JSON.stringify(aiRequest)}`);
-  const aiResponse = await rp.post({ body: aiRequest });
-  console.log(`(${id}) aiResponse: ${JSON.stringify(aiResponse)}`);
-  const alexaResponse = {
-    version: '1.0',
-    response: {},
-    shouldEndSession: false,
-  };
-  if (aiResponse.say) {
-    alexaResponse.response.outputSpeech = { type: 'PlainText', text: aiResponse.say };
-  }
-  if (aiResponse.display) {
-    alexaResponse.response.card = { type: 'Standard', title: aiResponse.display.title, text: aiResponse.display.text };
-  }
-  if (aiResponse.session) {
-    alexaResponse.sessionAttributes = aiResponse.session;
-  }
-  if (aiResponse.finishSession) {
-    alexaResponse.shouldEndSession = true;
-  }
-  console.log(`(${id}) alexaResponse: ${JSON.stringify(alexaResponse)}`);
-  res.send(alexaResponse);
-});
+const { app } = require('dexpress');
+const rp = require('request-promise').defaults({ json: true, url: process.env.AI_WEBHOOK_URL });
+require('dcontrollers')(app, [
+  require('./controller')({ rp }),
+]);
