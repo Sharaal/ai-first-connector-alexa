7de51a8ed7311fdcee8db7c606ba8699250942a6
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-06 13:41:33 +0200

    Add the opportunity to restrict the application

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,13 @@
-module.exports = ({ rp, secret }) =>
+module.exports = ({ applications, rp, secret }) =>
   ['post', ['/', require('body-parser').json(), async (req, res) => {
     const alexaRequest = req.body;
     let aiRequest, aiResponse, alexaResponse, error;
     try {
       aiRequest = require('./transformers/request')(alexaRequest, { secret });
       if (aiRequest) {
+        if (applications && !applications.includes(aiRequest.application)) {
+          throw new Error('incorrect application');
+        }
         aiResponse = await rp.post({ body: aiRequest });
         alexaResponse = require('./transformers/response')(aiResponse);
       }
