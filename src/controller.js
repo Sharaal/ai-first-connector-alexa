07a5e03d7793a50d6109f8379684b14a7e2df7c7
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-06 14:38:13 +0200

    Add logging for the jwt-token of the header

--- src/controller.js
+++ src/controller.js
@@ -5,23 +5,23 @@ module.exports = ({ applications, rp, secret }) =>
   ['post', ['/', require('body-parser').json(), async (req, res) => {
     const alexaRequest = req.body;
     let aiRequest, aiResponse, alexaResponse, error;
+    const application = _.get(alexaRequest, 'session.application.applicationId', '');
+    const headers = {};
+    if (secret) {
+      headers['jwt-token'] = jwt.sign({ application, connector: 'alexa' }, secret);
+    }
     try {
-      const application = _.get(alexaRequest, 'session.application.applicationId', '');
       if (applications && !applications.includes(application)) {
         throw new Error('incorrect application');
       }
       aiRequest = require('./transformers/request')(alexaRequest);
       if (aiRequest) {
-        const headers = {};
-        if (secret) {
-          headers['jwt'] = jwt.sign({ application, connector: 'alexa' }, secret);
-        }
         aiResponse = await rp.post({ body: aiRequest, headers });
         alexaResponse = require('./transformers/response')(aiResponse);
       }
     } catch (e) {
       error = e.message;
     }
-    console.log(`(${aiRequest.id}): ${JSON.stringify({ alexaRequest, aiRequest, aiResponse, alexaResponse, error })}`);
+    console.log(`(${aiRequest.id}): ${JSON.stringify({ alexaRequest, aiRequest, aiResponse, alexaResponse, error, headers })}`);
     res.send(alexaResponse);
   }]];
