b2757748356be91e38ad3e1b8f681b6fd47acabb
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-06 14:04:53 +0200

    Don't give the application ID to the webhook

--- src/controller.js
+++ src/controller.js
@@ -1,13 +1,16 @@
+const _ = require('lodash');
+
 module.exports = ({ applications, rp, secret }) =>
   ['post', ['/', require('body-parser').json(), async (req, res) => {
     const alexaRequest = req.body;
     let aiRequest, aiResponse, alexaResponse, error;
     try {
+      const application = _.get(alexaRequest, 'session.application.applicationId', '');
+      if (applications && !applications.includes(application)) {
+        throw new Error('incorrect application');
+      }
       aiRequest = require('./transformers/request')(alexaRequest, { secret });
       if (aiRequest) {
-        if (applications && !applications.includes(aiRequest.application)) {
-          throw new Error('incorrect application');
-        }
         aiResponse = await rp.post({ body: aiRequest });
         alexaResponse = require('./transformers/response')(aiResponse);
       }
