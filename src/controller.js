1b9f42021049bb9622a5258fa03da0627fcc8025
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 17:19:37 +0200

    Simplify by removing applications and secret

--- src/controller.js
+++ src/controller.js
@@ -1,27 +1,16 @@
-const _ = require('lodash');
-
-module.exports = ({ applications, rp, secret }) => [
+module.exports = ({ rp }) => [
   'post',
   [
     '/',
-    require('body-parser').json(),
     async (req, res) => {
       const alexaRequest = req.body;
       let aiRequest;
       let aiResponse;
       let alexaResponse;
       try {
-        const application = _.get(
-          alexaRequest,
-          'session.application.applicationId',
-          ''
-        );
-        if (applications && !applications.includes(application)) {
-          throw new Error('incorrect application');
-        }
         aiRequest = require('./transformers/request')(alexaRequest);
         if (aiRequest) {
-          aiResponse = await rp.post({ body: aiRequest, headers: { secret } });
+          aiResponse = await rp.post({ body: aiRequest });
           alexaResponse = require('./transformers/response')(aiResponse);
         }
         console.log(
