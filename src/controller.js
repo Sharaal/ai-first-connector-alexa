697c85e23c7a5d41809819da2436c07fc6572dd5
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 15:10:25 +0200

    Add dprettier

--- src/controller.js
+++ src/controller.js
@@ -1,25 +1,45 @@
 const _ = require('lodash');
 
-module.exports = ({ applications, rp, secret }) =>
-  ['post', ['/', require('body-parser').json(), async (req, res) => {
-    const alexaRequest = req.body;
-    let aiRequest;
-    let aiResponse;
-    let alexaResponse;
-    try {
-      const application = _.get(alexaRequest, 'session.application.applicationId', '');
-      if (applications && !applications.includes(application)) {
-        throw new Error('incorrect application');
+module.exports = ({ applications, rp, secret }) => [
+  'post',
+  [
+    '/',
+    require('body-parser').json(),
+    async (req, res) => {
+      const alexaRequest = req.body;
+      let aiRequest;
+      let aiResponse;
+      let alexaResponse;
+      try {
+        const application = _.get(
+          alexaRequest,
+          'session.application.applicationId',
+          ''
+        );
+        if (applications && !applications.includes(application)) {
+          throw new Error('incorrect application');
+        }
+        aiRequest = require('./transformers/request')(alexaRequest);
+        if (aiRequest) {
+          aiResponse = await rp.post({ body: aiRequest, headers: { secret } });
+          alexaResponse = require('./transformers/response')(aiResponse);
+        }
+        console.log(
+          JSON.stringify({ alexaRequest, aiRequest, aiResponse, alexaResponse })
+        );
+      } catch (e) {
+        const error = e.message;
+        console.error(
+          JSON.stringify({
+            error,
+            alexaRequest,
+            aiRequest,
+            aiResponse,
+            alexaResponse,
+          })
+        );
       }
-      aiRequest = require('./transformers/request')(alexaRequest);
-      if (aiRequest) {
-        aiResponse = await rp.post({ body: aiRequest, headers: { secret } });
-        alexaResponse = require('./transformers/response')(aiResponse);
-      }
-      console.log(JSON.stringify({ alexaRequest, aiRequest, aiResponse, alexaResponse }));
-    } catch (e) {
-      const error = e.message;
-      console.error(JSON.stringify({ error, alexaRequest, aiRequest, aiResponse, alexaResponse }));
-    }
-    res.send(alexaResponse);
-  }]];
+      res.send(alexaResponse);
+    },
+  ],
+];
--- src/web.js
+++ src/web.js
@@ -1,12 +1,14 @@
 require('dotenv-safe').config();
 
-require('dcontrollers')(
-  require('dexpress')(),
-  [
-    require('./controller')({
-      applications: process.env.AI_APPLICATIONS ? process.env.AI_APPLICATIONS.split(', ') : null,
-      rp: require('request-promise').defaults({ json: true, url: process.env.AI_WEBHOOK_URL }),
-      secret: process.env.AI_WEBHOOK_SECRET,
+require('dcontrollers')(require('dexpress')(), [
+  require('./controller')({
+    applications: process.env.AI_APPLICATIONS
+      ? process.env.AI_APPLICATIONS.split(', ')
+      : null,
+    rp: require('request-promise').defaults({
+      json: true,
+      url: process.env.AI_WEBHOOK_URL,
     }),
-  ]
-);
+    secret: process.env.AI_WEBHOOK_SECRET,
+  }),
+]);
